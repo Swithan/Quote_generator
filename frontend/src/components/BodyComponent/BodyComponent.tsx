b8f512f1fca988e39bb3821d6349229c6dfc34ec
Swithan22 <nathandebongnie@gmail.com>
2024-02-04 15:35:54 +0100

    Handle backend call in frontend app
    Use usestate to avoid repeating same quote

--- frontend/src/components/BodyComponent/BodyComponent.tsx
+++ frontend/src/components/BodyComponent/BodyComponent.tsx
@@ -1,12 +1,34 @@
-import React, { FC } from 'react';
+import React, { useState } from 'react';
 import styles from './BodyComponent.module.css';
 
-interface BodyComponentProps {}
 
-const BodyComponent: FC<BodyComponentProps> = () => (
-  <div className={styles.BodyComponent} data-testid="BodyComponent">
-    BodyComponent Component
-  </div>
-);
+function BodyComponent ()  {
+  const [quote, setQuote] = useState({"content": "Welcome to the random quote generator.", "author": "Get a quote here bellow"});
+  const [lastQuotes, setLastQuotes] = React.useState<any[]>([]);
+
+  const getQuote = async () => {
+      fetch("http://localhost:4200/quote")
+          .then((response) => response.json())
+          .then((quote) => {
+            setLastQuotes(lastQuotes => [...lastQuotes, quote])
+            if (lastQuotes.length > 100) {
+              let [, ...all] = lastQuotes;
+              setLastQuotes(lastQuotes => all)
+            }
+            if (quote in lastQuotes) {
+              getQuote();
+            } else {
+              setQuote(quote);
+            }
+          });
+  };
+
+
+  return (<div className={styles.BodyComponent} data-testid="BodyComponent">
+    <p>"{quote.content}"</p>
+    <p>~ {quote.author} ~</p>
+    <button onClick={getQuote}>Next quote</button>
+  </div>);
+};
 
 export default BodyComponent;
