5b07564ef6644773ea7a9bac1cb4bda58fce0117
Swithan22 <nathandebongnie@gmail.com>
2024-02-04 16:54:09 +0100

    Frontend error handling

--- frontend/src/components/BodyComponent/BodyComponent.tsx
+++ frontend/src/components/BodyComponent/BodyComponent.tsx
@@ -6,26 +6,38 @@ function BodyComponent ()  {
   const [quote, setQuote] = useState({"content": "Welcome to the random quote generator.", "author": "Get a quote here bellow"});
   const [lastQuotes, setLastQuotes] = React.useState<any[]>([]);
 
+  const [error, setError] = useState(null)
   const getQuote = async () => {
       fetch("http://localhost:4200/quote")
           .then((response) => response.json())
           .then((quote) => {
-            setLastQuotes(lastQuotes => [...lastQuotes, quote])
-            if (lastQuotes.length > 100) {
-              let [, ...all] = lastQuotes;
-              setLastQuotes(all)
-            }
-            if (quote in lastQuotes) {
-              getQuote();
+            if (quote.error) {
+              setError(quote.error);
             } else {
-              setQuote(quote);
+              setError(null)
+              setLastQuotes(lastQuotes => [...lastQuotes, quote])
+              if (lastQuotes.length > 100) {
+                let [, ...all] = lastQuotes;
+                setLastQuotes(all)
+              }
+              if (quote in lastQuotes) {
+                getQuote();
+              } else {
+                setQuote(quote);
+              }
             }
           });
   };
   const getNewQuote = () => {
     return getQuote();
   };
-
+  if (error) {
+    return (<div className={styles.BodyComponent} data-testid="BodyComponent">
+    <p className={styles.quote}>An error occured. Please try again</p>
+    <button onClick={getNewQuote}>Next quote</button>
+  </div>
+    )
+  }
   return (<div className={styles.BodyComponent} data-testid="BodyComponent">
     <p className={styles.quote}>"{quote.content}"</p>
     <p className={styles.author}>~ {quote.author} ~</p>
